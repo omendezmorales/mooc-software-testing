897804905baf3f6d3c9dd4cffc9d018ee848f67e
Orlando <mmoa33@hotmail.com>
2018-11-30 15:53:38 -0300

    Added initialize method to avoid creating a new RomanNumeral in each test method.

--- src/test/java/tudelft/roman/RomanNumeralTest.java
+++ src/test/java/tudelft/roman/RomanNumeralTest.java
@@ -1,35 +1,44 @@
 package tudelft.roman;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class RomanNumeralTest {
 
 
+    private RomanNumeral roman;
+
+    @BeforeEach
+    public void initialize() {
+        this.roman = new RomanNumeral();
+    }
+
     @Test
     public void singleNumber() {
-        RomanNumeral roman = new RomanNumeral();
         int result = roman.convert("I");
         Assertions.assertEquals(1, result);
     }
 
     @Test
+    public void DoubleNumber() {
+        int result = roman.convert("VI");
+        Assertions.assertEquals(6, result);
+    }
+    @Test
     public void numberWithManyDigits() {
-        RomanNumeral roman = new RomanNumeral();
         int result = roman.convert("VIII");
         Assertions.assertEquals(8, result);
     }
 
     @Test
     public void numberWithSubtractiveNotation() {
-        RomanNumeral roman = new RomanNumeral();
         int result = roman.convert("IV");
         Assertions.assertEquals(4, result);
     }
 
     @Test
     public void numberWithAndWithoutSubtractiveNotation() {
-        RomanNumeral roman = new RomanNumeral();
         int result = roman.convert("XLIV");
         Assertions.assertEquals(44, result);
     }
